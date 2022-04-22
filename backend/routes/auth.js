30cdb1ddbf65a2870a8334dcfe6b61db66f22f57
GabrielConforte <82458582+GabrielConforte@users.noreply.github.com>
2022-04-21 21:50:57 -0300

    añadio proyecto

---
+++ backend/routes/auth.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const passport = require("passport");
+const {client} = require("../config/index")
+
+router.get("/facebook", passport.authenticate("facebook", { scope: ["profile"] }));
+
+router.get("/login/failed", (req, res) => {
+    res.send("Login failed");
+});
+
+router.get("/login/success", (req, res) => {
+    if(req.user){
+        res.status(200).json(
+            {
+                user: req.user,
+                message: "Login success",
+                redirect: client.client_url,
+                cookies: req.cookies
+            }
+        )
+    }
+});
+
+router.get("facebook/callback", passport.authenticate("facebook", {
+    successRedirect: client.client_url,
+    failureRedirect: "/login/failed"
+}));
+
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect(client.client_url);
+});
+
+module.exports = router;
